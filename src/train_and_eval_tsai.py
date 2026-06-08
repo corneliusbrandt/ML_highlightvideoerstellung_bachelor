0ca6efb85d31884a54960dbe0fb214d0311d4137
corneliusbrandt <bc7970@mci4me.at>
2026-06-08 12:44:20 +0200

    Res net 1d und Omni Scale CNN gestestet

--- src/train_and_eval_tsai.py
+++ src/train_and_eval_tsai.py
@@ -8,34 +8,24 @@ from helper import calculate_class_weights
 
 
 # -----------------------------
-# Daten laden
+# load data
 # -----------------------------
 X_train, y_train = load_data("datasets_output/train_dataset_augmented_binary.npz")
 X_val, y_val = load_data("datasets_output/val_dataset_binary.npz")
 
 
 # -----------------------------
-# Shape anpassen
+# Adjust shape
 # -----------------------------
-# Von: (samples, time_steps, channels)
-# Zu:  (samples, channels, time_steps)
+# from: (samples, time_steps, channels)
+# to:  (samples, channels, time_steps)
 X_train = np.transpose(X_train, (0, 2, 1))
 X_val = np.transpose(X_val, (0, 2, 1))
 
 
 # -----------------------------
-# Daten bereinigen
+# Print out data shapes and class distribution
 # -----------------------------
-# X_train = np.nan_to_num(X_train, nan=0.0, posinf=0.0, neginf=0.0)
-# X_val = np.nan_to_num(X_val, nan=0.0, posinf=0.0, neginf=0.0)
-
-# X_train = np.ascontiguousarray(X_train, dtype=np.float32)
-# X_val = np.ascontiguousarray(X_val, dtype=np.float32)
-
-# y_train = np.asarray(y_train, dtype=np.int64)
-# y_val = np.asarray(y_val, dtype=np.int64)
-
-
 print("X_train:", X_train.shape, X_train.dtype, X_train.flags["C_CONTIGUOUS"])
 print("y_train:", y_train.shape, y_train.dtype)
 print("Klassen Train:", np.unique(y_train, return_counts=True))
@@ -46,8 +36,9 @@ print("Klassen Val:", np.unique(y_val, return_counts=True))
 
 
 # -----------------------------
-# Train/Val zusammenführen
+# Train/Val 
 # -----------------------------
+# tsai works with combined datasets and uses indices to split them -> Train and Val are NOT beeing combined
 X = np.concatenate([X_train, X_val], axis=0)
 y = np.concatenate([y_train, y_val], axis=0)
 
@@ -58,7 +49,7 @@ splits = (train_idxs, val_idxs)
 
 
 # -----------------------------
-# DataLoaders erstellen
+# create dataloaders
 # -----------------------------
 batch_size = 32
 
@@ -67,22 +58,22 @@ dls = get_ts_dls(
     y,
     splits=splits,
     bs=batch_size,
-    tfms=[None, TSCategorize()]
+    tfms=[None, TSCategorize()],
+    shuffle_train=True
 )
 
 
 # -----------------------------
-# Modell erstellen
+# create model
 # -----------------------------
 n_channels = X_train.shape[1]
 n_classes = len(np.unique(y_train))
 
-class_weights = calculate_class_weights(y_train, scaling_factor=2.5, min_weight=1.5)
-loss_function = FocalLoss(gamma=3, alpha=class_weights, task_type='multi-class', num_classes=n_classes)
 
 model = InceptionTime(
     c_in=n_channels,
-    c_out=n_classes
+    c_out=n_classes,
+    seq_len=X_train.shape[2]
 )
 
 print("n_channels:", n_channels)
@@ -90,27 +81,34 @@ print("n_classes:", n_classes)
 
 
 # -----------------------------
-# Learner erstellen
+# create Learner
 # -----------------------------
+
+class_weights = calculate_class_weights(y_train, scaling_factor=2.5, min_weight=1.5)
+loss_function = FocalLoss(gamma=3, alpha=class_weights, task_type='multi-class', num_classes=n_classes)
+
 learn = Learner(
     dls,
     model,
     loss_func=loss_function,
-    metrics=accuracy
+    opt_func=Adam,
+    metrics=[accuracy, F1Score(average='binary'), Precision(average='binary'), Recall(average='binary')],
+    path=Path("src/Models"),
+    cbs = [SaveModel(fname="best_model_INCEPTIONTIME_tsai", verbose=True)]
 )
 
 
 # -----------------------------
-# Training
+# Train
 # -----------------------------
 learn.fit_one_cycle(
-    n_epoch=30,
-    lr_max=1e-3
+    n_epoch=20,
+    lr_max=1e-4
 )
 
 
 # -----------------------------
-# Vorhersage auf Validation Set
+# Validation
 # -----------------------------
 preds, targets = learn.get_preds(ds_idx=1)
 
@@ -121,30 +119,24 @@ y_pred = np.argmax(probs, axis=1)
 
 
 # -----------------------------
-# Auswertung mit argmax
+# Prediction wih argmax
 # -----------------------------
-print("\n--- Auswertung mit argmax ---")
+print("\n--- Prediction mit argmax ---")
 print(confusion_matrix(y_true, y_pred))
 print(classification_report(y_true, y_pred, zero_division=0))
 print("F1:", f1_score(y_true, y_pred, zero_division=0))
 
 
 # -----------------------------
-# Auswertung mit Threshold
+# prediction with threshold
 # -----------------------------
-threshold = 0.8
+threshold = 0.5
 
 event_probs = probs[:, 1]
 y_pred_threshold = (event_probs > threshold).astype(int)
 
-print("\n--- Auswertung mit Threshold ---")
+print("\n--- Prediction with Threshold ---")
 print("Threshold:", threshold)
 print(confusion_matrix(y_true, y_pred_threshold))
 print(classification_report(y_true, y_pred_threshold, zero_division=0))
 print("F1:", f1_score(y_true, y_pred_threshold, zero_division=0))
-
-
-# -----------------------------
-# Modell speichern
-# -----------------------------
-learn.export("inception_time_tsai.pkl")
